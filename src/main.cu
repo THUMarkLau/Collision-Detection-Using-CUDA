469d8aaf0d63dfce12f9b828cb758ce1678fec5a
THUMarkLau <liuxuxin@outlook.com>
2020-12-04 18:21:53 +0800

    configuration

---
+++ src/main.cu
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+// main run in host
+int main(void) {
+	
+	return 0;
+}
